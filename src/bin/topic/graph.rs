fe20444a5193eee264ea808b1373eca7bb126736
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-12-07 15:43:29 -0500

    solve kattis_wheresmyinternet

--- src/bin/topic/graph.rs
+++ src/bin/topic/graph.rs
@@ -7,14 +7,16 @@ use std::io;
 fn main() {
     let input = io::read_to_string(io::stdin()).unwrap();
     let output = kattis_wheresmyinternet(&input);
-    match output {
-        Some(cc_flattened) => cc_flattened.iter().for_each(|v| println!("{v}")),
-        None => println!("Connected"),
+    if output.len() == 0 {
+        println!("Connected")
+    } else {
+        output.iter().for_each(|v| println!("{v}"));
     }
 }
 
 // rust: .take()/.skip() for filter, .nth() for selection
-fn kattis_wheresmyinternet(input: &str) -> Option<Vec<usize>> {
+// blunder: (1..=V).fold(|v|) is not sequential??
+fn kattis_wheresmyinternet(input: &str) -> Vec<usize> {
     let (V, _) = input
         .lines()
         .take(1)
@@ -45,39 +47,19 @@ fn kattis_wheresmyinternet(input: &str) -> Option<Vec<usize>> {
             p
         });
 
-    fn dfs(al: &Vec<Vec<usize>>, v: usize, seen: &mut [bool], path: &mut Vec<usize>) -> () {
-        let ((), ()) = (path.push(v), seen[v] = true); // visit and mark
+    fn dfs(al: &Vec<Vec<usize>>, v: usize, seen: &mut [bool]) -> () {
+        seen[v] = true; // mark, no visit
         for &w in &al[v] {
             if !seen[w] {
-                dfs(al, w, seen, path); // recurse
+                dfs(al, w, seen); // recurse
             }
         }
     }
 
-    let (ccs, _) = (1..=V).fold(
-        (Vec::new(), vec![false; V + 1]),
-        |(mut ccs, mut seen), v| {
-            if !seen[v] {
-                let mut cc = Vec::new();
-                dfs(&al, v, &mut seen, &mut cc);
-                ccs.push(cc); // update
-            };
-
-            (ccs, seen)
-        },
-    );
-
-    if ccs.len() == 1 {
-        None
-    } else {
-        let ccs_flattened = ccs
-            .iter()
-            .skip(1)
-            .flat_map(|cc| cc.iter().copied())
-            .collect::<Vec<_>>();
-
-        Some(ccs_flattened)
-    }
+    let mut seen = vec![false; V + 1];
+    dfs(&al, 1, &mut seen);
+    let not_seen = (1..=V).filter(|&v| !seen[v]).collect::<Vec<_>>();
+    not_seen
 }
 
 fn kattis_dominoes2(input: &str) -> u32 {
@@ -160,21 +142,29 @@ mod tests_traverals {
 3 4
 5 6
 ";
-        let output = kattis_wheresmyinternet(input).unwrap();
+        let output = kattis_wheresmyinternet(input);
         assert_eq!(output, vec![5, 6]);
 
         let input = "2 1
 2 1
 ";
         let output = kattis_wheresmyinternet(input);
-        assert_eq!(output, None);
+        assert_eq!(output, vec![]);
 
         let input = "4 3
 2 3
 4 2
 3 4
 ";
-        let output = kattis_wheresmyinternet(input).unwrap();
+        let output = kattis_wheresmyinternet(input);
+        assert_eq!(output, Vec::from([2, 3, 4]));
+
+        let input = "4 3
+2 3
+4 2
+3 4
+";
+        let output = kattis_wheresmyinternet(input);
         assert_eq!(output, Vec::from([2, 3, 4]));
     }
 
