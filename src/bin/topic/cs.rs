dd6c2930756654b664b9d7f4f57c6c64e53c8f00
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-12-09 10:16:32 -0500

    move files

--- src/bin/topic/cs.rs
+++ src/bin/topic/cs.rs
@@ -2,12 +2,6 @@ mod test_precalc {}
 // kattis_cardtrick2
 // kattis_foolingaround
 // kattis_sgcoin
-// kattis_4thought
-// kattis_chocolates
-// kattis_lastfactorialdigti
-// kattis_luckynumber
-// kattis_mancala
-// kattis_primematrix
 
 mod test_iterative {}
 // double loop
@@ -16,19 +10,6 @@ mod test_iterative {}
 // kattis_blackfriday
 // kattis_closestsums
 // kattis_golumbrulers
-// kattis_8queens
-// kattis_antiarithmetic
-// kattis_bestrelayteam
-// kattis_bikegears
-// kattis_kafkaesque
-// kattis_liga
-// kattis_peg
-// kattis_putovanje
-// kattis_reduction
-// kattis_register
-// kattis_summertrip
-// kattis_telephones
-// kattis_tourdefrance
 
 // triple loop
 // ------------------
@@ -41,68 +22,37 @@ mod test_iterative {}
 // kattis_calculatingdartscores
 // kattis_lektira
 // kattis_tautology
-// kattis_goblinguards
-// kattis_misa
-// kattis_medals
 
 // permutations
 // ------------------
 // kattis_dancerecital
 // kattis_dreamer
 // kattis_veci
-// kattis_classpicture
-// kattis_towering
-// kattis_victorythroughsynergy
 
 // combinations
 // kattis_geppetto
 // kattis_squaredeal
 // kattis_zagrade
-// kattis_buildingboundaries
-// kattis_doubleplusgood
-// kattis_perket
 
 mod test_mathsim {}
 // kattis_growlinggears
 // kattis_trollhunt
 // kattis_videospeedup
-// kattis_aboveaverage
-// kattis_dicecup
-// kattis_harshadnumbers
-// kattis_socialrunning
-// kattis_sodaslurper
-// kattis_somesum
-// kattis_sumoftheothers
-// kattis_tri
-// kattis_zamka
 // kattis_crackingrsa
 // kattis_falling
 // kattis_thanosthehero
-// kattis_disgruntledjudge
-// kattis_houselawn
-// kattis_liopschitzconstant
-// kattis_milestones
-// kattis_repeatingdecimal
-// kattis_robotopia
-// kattis_stopcounting
-// kattis_trick
 
 mod test_josephus {}
 // kattis_eenymeeny
 // kattis_musicalchairs
 // kattis_toys
-// kattis_cocunut
 
 mod test_backtracking {}
 // kattis_goodmorning
 // kattis_natjecanje
 // kattis_paintings
-// kattis_draughts
 // kattis_dobra
 // kattis_fruitbaskets
 // kattis_pagelayout
-// kattis_carvet
-// kattis_primes
-// kattis_solitaire
 
 fn main() {}
